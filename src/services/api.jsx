038d1e8b13ced2ef80ac6295ea408175ab9f9edd
priyadhashwanth <priyadharshini80302gmail.com>
2026-04-20 12:08:45 +0530

    cors added

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://hostel-backend-sxqd.onrender.com/api"
 });
 
 // attach token automatically
